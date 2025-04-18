0a9da379354e069b615cf9371d60e949631b899e
Yogesh Arya <yogesh084arya@gmail.com>
2025-04-18 10:44:19 +0530

    update hero section

--- client/src/app/(main)/page.js
+++ client/src/app/(main)/page.js
@@ -1,45 +1,9 @@
-import Link from "next/link";
-import { Button } from "../../components/ui/button";
-import Image from "next/image";
+import HeroSection from "./_components/HeroSection";
 
 export default function Home() {
   return (
-    <section className="pt-40 pb-20 px-4">
-      <div className="container mx-auto text-center">
-        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 bg-gradient-to-br from-blue-600 to-purple-600 font-extrabold tracking-tighter pr-2 pb-2 text-transparent bg-clip-text">
-          Manage Your Finances <br /> with{" "}
-          <span className="text-black">Personal </span>
-          <span className="text-green-500">Finance</span>
-        </h1>
-        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
-          A financial management platform that helps you track, analyze, and
-          optimize your spending with real-time insights.
-        </p>
-        <div className="flex justify-center space-x-4 mb-4">
-          <Link href="/dashboard">
-            <Button size="lg" className="px-8">
-              Get Started
-            </Button>
-          </Link>
-          <Link href="/dashboard">
-            <Button size="lg" variant="outline" className="px-8">
-              Watch Demo
-            </Button>
-          </Link>
-        </div>
-        <div className="hero-image-wrapper mt-5 md:mt-0">
-          <div className="hero-image">
-            <Image
-              src="/Finance.jpg"
-              width={1280}
-              height={720}
-              alt="Dashboard Preview"
-              className="rounded-lg shadow-2xl border mx-auto"
-              priority
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+    <div>
+      <HeroSection />
+    </div>
   );
 }
